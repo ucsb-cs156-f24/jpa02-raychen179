798120160a56ab8c7e9a01a5c13346cb17c34d13
Raymond Chen <raymondchen@ucsb.edu>
2024-10-09 11:23:15 -0700

    xy - updated with new tests

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,8 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-f24-teams>
-        // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Raymond";
     }
 
     /**
@@ -29,8 +26,8 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+
+        return "raychen179";
     }
 
     /**
@@ -39,14 +36,14 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("f24-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+
+        Team team = new Team("f24-01");
+        team.addMember("Jun");
+        team.addMember("Raymond");
+        team.addMember("Scott");
+        team.addMember("Vincent");
+        team.addMember("Viraj");
+        team.addMember("Cameron");
         return team;
     }
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -22,12 +22,31 @@ public class DeveloperTest {
 
     @Test
     public void getName_returns_correct_name() {
-        // TODO: Replace Chris G. with your name as shown on
-        // <https://bit.ly/cs156-f24-teams>
-        assertEquals("Chris G.", Developer.getName());
+
+        assertEquals("Raymond", Developer.getName());
     }
 
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+
+        assertEquals("raychen179", Developer.getGithubId());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("f24-01", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Jun"),"Team should contain Jun");
+        assertTrue(t.getMembers().contains("Raymond"),"Team should contain Raymond");
+        assertTrue(t.getMembers().contains("Scott"),"Team should contain Scott");
+        assertTrue(t.getMembers().contains("Vincent"),"Team should contain Vincent");
+        assertTrue(t.getMembers().contains("Viraj"),"Team should contain Viraj");
+        assertTrue(t.getMembers().contains("Cameron"),"Team should contain Cameron");
+    }
 
 }
